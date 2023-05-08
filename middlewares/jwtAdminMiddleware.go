87b48189062cfdb2dd756f0117734032b09e54b9
Galuh Trihanggara <124585891+GaluhTrihanggara@users.noreply.github.com>
2023-05-09 03:17:45 +0700

    push file middleware

---
+++ middlewares/jwtAdminMiddleware.go
@@ -0,0 +1,28 @@
+package middlewares
+
+import (
+	"MiniProject/constants"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/labstack/echo/v4"
+)
+
+func CreateAdminToken(adminId int) (string, error) {
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["adminId"] = adminId
+	claims["exp"] = time.Now().Add(time.Hour * 5).Unix() // Token expires after 5 hours
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(constants.SECRET_JWT))
+}
+func ExtractTokenAdminId(e echo.Context) int {
+	admin := e.Get("admin").(*jwt.Token)
+	if admin.Valid {
+		claims := admin.Claims.(jwt.MapClaims)
+		adminId := claims["adminId"].(int)
+		return adminId
+	}
+	return 0
+}
