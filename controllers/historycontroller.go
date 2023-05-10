420005a3b01706031bc7509811e641f63b61bc1d
Galuh Trihanggara <124585891+GaluhTrihanggara@users.noreply.github.com>
2023-05-10 18:02:18 +0700

    Update historycontroller.go

--- controllers/historycontroller.go
+++ controllers/historycontroller.go
@@ -11,15 +11,15 @@ import (
 
 // CreateHistoryController will create a new history data
 func CreateHistoryController(c echo.Context) error {
-	var history models.History
-	if err := c.Bind(&history); err != nil {
+	var histories models.Histories
+	if err := c.Bind(&histories); err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]interface{}{
 			"status": "failed to create history data",
 			"error":  err.Error(),
 		})
 	}
 
-	if err := config.DB.Create(&history).Error; err != nil {
+	if err := config.DB.Create(&histories).Error; err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]interface{}{
 			"status": "failed to create history data",
 			"error":  err.Error(),
@@ -28,14 +28,14 @@ func CreateHistoryController(c echo.Context) error {
 
 	return c.JSON(http.StatusCreated, map[string]interface{}{
 		"status":  "success create history data",
-		"history": history,
+		"history": histories,
 	})
 }
 
 // GetHistoriesController will get all history data
-func GetHistorysController(c echo.Context) error {
-	var historys []models.History
-	if err := config.DB.Find(&historys).Error; err != nil {
+func GetHistoriesController(c echo.Context) error {
+	var histories []models.Histories
+	if err := config.DB.Find(&histories).Error; err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]interface{}{
 			"status": "failed to get history data",
 			"error":  err.Error(),
@@ -44,60 +44,12 @@ func GetHistorysController(c echo.Context) error {
 
 	return c.JSON(http.StatusOK, map[string]interface{}{
 		"status":   "success get history data",
-		"historys": historys,
-	})
-}
-
-// GetHistoryController will get a single history data by id
-func GetHistoryController(c echo.Context) error {
-	id, err := strconv.Atoi(c.Param("id"))
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]interface{}{
-			"status": "invalid parameter",
-			"error":  err.Error(),
-		})
-	}
-
-	var history models.History
-	if err := config.DB.First(&history, id).Error; err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]interface{}{
-			"status": "failed to get history data",
-			"error":  err.Error(),
-		})
-	}
-
-	return c.JSON(http.StatusOK, map[string]interface{}{
-		"status":  "success get history data",
-		"history": history,
-	})
-}
-
-// GetHistoriesByUserController will get all history data by user id
-func GetHistoryByUserController(c echo.Context) error {
-	userID, err := strconv.Atoi(c.Param("id"))
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]interface{}{
-			"status": "invalid parameter",
-			"error":  err.Error(),
-		})
-	}
-
-	var history []models.History
-	if err := config.DB.Where("user_id = ?", userID).Find(&history).Error; err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]interface{}{
-			"status": "failed to get history data",
-			"error":  err.Error(),
-		})
-	}
-
-	return c.JSON(http.StatusOK, map[string]interface{}{
-		"status":  "success get history data",
-		"history": history,
+		"historys": histories,
 	})
 }
 
 // GetHistoriesByPaymentController will get all history data by payment id
-func GetHistoryByPaymentController(c echo.Context) error {
+func GetHistoriesByPaymentController(c echo.Context) error {
 	paymentID, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]interface{}{
@@ -106,8 +58,8 @@ func GetHistoryByPaymentController(c echo.Context) error {
 		})
 	}
 
-	var history []models.History
-	if err := config.DB.Where("payment_id = ?", paymentID).Find(&history).Error; err != nil {
+	var histories []models.Histories
+	if err := config.DB.Where("payment_id = ?", paymentID).Find(&histories).Error; err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]interface{}{
 			"status": "failed to get history data",
 			"error":  err.Error(),
@@ -116,7 +68,7 @@ func GetHistoryByPaymentController(c echo.Context) error {
 
 	return c.JSON(http.StatusOK, map[string]interface{}{
 		"status":  "success get history data",
-		"history": history,
+		"history": histories,
 	})
 }
 
@@ -129,22 +81,22 @@ func UpdateHistoryController(c echo.Context) error {
 			"error":  err.Error(),
 		})
 	}
-	var history models.History
-	if err := config.DB.First(&history, id).Error; err != nil {
+	var histories models.Histories
+	if err := config.DB.First(&histories, id).Error; err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]interface{}{
 			"status": "failed to update history data",
 			"error":  err.Error(),
 		})
 	}
 
-	if err := c.Bind(&history); err != nil {
+	if err := c.Bind(&histories); err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]interface{}{
 			"status": "failed to update history data",
 			"error":  err.Error(),
 		})
 	}
 
-	if err := config.DB.Save(&history).Error; err != nil {
+	if err := config.DB.Save(&histories).Error; err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]interface{}{
 			"status": "failed to update history data",
 			"error":  err.Error(),
@@ -153,7 +105,7 @@ func UpdateHistoryController(c echo.Context) error {
 
 	return c.JSON(http.StatusOK, map[string]interface{}{
 		"status":  "success update history data",
-		"history": history,
+		"history": histories,
 	})
 }
 
@@ -167,15 +119,15 @@ func DeleteHistoryController(c echo.Context) error {
 		})
 	}
 
-	var history models.History
-	if err := config.DB.First(&history, id).Error; err != nil {
+	var histories models.Histories
+	if err := config.DB.First(&histories, id).Error; err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]interface{}{
 			"status": "failed to delete history data",
 			"error":  err.Error(),
 		})
 	}
 
-	if err := config.DB.Delete(&history).Error; err != nil {
+	if err := config.DB.Delete(&histories).Error; err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]interface{}{
 			"status": "failed to delete history data",
 			"error":  err.Error(),
@@ -184,6 +136,6 @@ func DeleteHistoryController(c echo.Context) error {
 
 	return c.JSON(http.StatusOK, map[string]interface{}{
 		"status":  "success delete history data",
-		"history": history,
+		"history": histories,
 	})
 }
