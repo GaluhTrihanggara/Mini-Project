0271d04f08b5140eccf537ba9953ccae83573fc3
Galuh Trihanggara <124585891+GaluhTrihanggara@users.noreply.github.com>
2023-05-10 18:12:37 +0700

    Update routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -12,37 +12,46 @@ func New() *echo.Echo {
 	// create a new echo instance
 	e := echo.New()
 	jwtMiddleware := middleware.JWT([]byte(constants.SECRET_JWT))
-	// Route / to handler function
-	e.GET("/users", controllers.GetUsersController, jwtMiddleware)
-	e.GET("/users/:id", controllers.GetUserController, jwtMiddleware)
+	// Route /users to handler functions for user authentication and authorization
 	e.POST("/users", controllers.CreateUserController)
 	e.POST("/users/login", controllers.LoginUserController)
-	e.DELETE("/users/:id", controllers.DeleteUserController, jwtMiddleware)
-	e.PUT("/users/:id", controllers.UpdateUserController, jwtMiddleware)
+	// Route /users/:id to handler functions for updating and deleting a user
+	userGroup := e.Group("/users/:id", jwtMiddleware)
+	userGroup.GET("", controllers.GetUserController)
+	userGroup.PUT("", controllers.UpdateUserController)
+	userGroup.DELETE("", controllers.DeleteUserController)
+	userGroup.GET("/bills/:id", controllers.GetBillController, jwtMiddleware)
+	userGroup.GET("/payments/:id", controllers.GetPaymentController, jwtMiddleware)
+	userGroup.GET("/histories/:id", controllers.GetHistoriesByPaymentController)
 
-	// Route for admin login using JWT
-
-	e.GET("/admin/:id", controllers.GetAdminController, jwtMiddleware)
-	e.GET("/admin", controllers.GetAdminsController, jwtMiddleware)
+	// Route /users to handler functions for user authentication and authorization
 	e.POST("/admin/login", controllers.LoginAdminController)
-	e.POST("/admin", controllers.CreateAdminController)
-	e.PUT("/admin/:id", controllers.UpdateAdminController, jwtMiddleware)
-	e.DELETE("/admin/:id", controllers.DeleteAdminController, jwtMiddleware)
+	// group routes for admin
+	adminGroup := e.Group("/admin", jwtMiddleware)
+	adminGroup.GET("", controllers.GetAdminsController)
+	adminGroup.GET("/:id", controllers.GetAdminController)
+	adminGroup.POST("", controllers.CreateAdminController)
+	adminGroup.PUT("/:id", controllers.UpdateAdminController)
+	adminGroup.DELETE("/:id", controllers.DeleteAdminController)
+
+	// Route /bills to handler function
+	adminGroup.POST("/bills", controllers.CreateBillController)
+	adminGroup.GET("/bills", controllers.GetBillsController)
+	adminGroup.GET("/bills/:id", controllers.GetBillController)
+	adminGroup.PUT("/bills/:id", controllers.UpdateBillController)
+	adminGroup.DELETE("/bills/:id", controllers.DeleteBillController)
 
 	// Route /payments to handler function
-	e.POST("/payments", controllers.CreatePaymentController)
-	e.GET("/payments", controllers.GetPaymentsController)
-	e.GET("/payment/:id", controllers.GetPaymentController)
-	e.PUT("/payments/:id", controllers.UpdatePaymentController)
-	e.DELETE("/payments/:id", controllers.DeletePaymentController)
+	adminGroup.POST("/payments", controllers.CreatePaymentController)
+	adminGroup.GET("/payments", controllers.GetPaymentsController)
+	adminGroup.GET("/payments/:id", controllers.GetPaymentController)
+	adminGroup.PUT("/payments/:id", controllers.UpdatePaymentController)
+	adminGroup.DELETE("/payments/:id", controllers.DeletePaymentController)
 
-	// History endpoints
-	e.POST("/history", controllers.CreateHistoryController)
-	e.GET("/history", controllers.GetHistorysController)
-	e.GET("/history/:id", controllers.GetHistoryController)
-	e.GET("/history/user/:id", controllers.GetHistoryByUserController)
-	e.GET("/history/payment/:id", controllers.GetHistoryByPaymentController)
-	e.PUT("/history/:id", controllers.UpdateHistoryController)
-	e.DELETE("/history/:id", controllers.DeleteHistoryController)
+	// Group routes for histories accessible only by admin
+	adminGroup.GET("/history/payment/:id", controllers.GetHistoriesByPaymentController)
+	adminGroup.POST("/history", controllers.CreateHistoryController)
+	adminGroup.PUT("/history/:id", controllers.UpdateHistoryController)
+	adminGroup.DELETE("/history/:id", controllers.DeleteHistoryController)
 	return e
 }
